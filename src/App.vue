e1162aa3bd17808deeac1e843a54bb787a2c88ae
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-06-15 10:22:50 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container fluid style="width: 600px">
+      <div class="mb-5">
+        <h1>bezkoder.com</h1>
+        <h2>Vuetify Image Upload with Preview</h2>
+      </div>
+
+      <upload-image></upload-image>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadImage from "./components/UploadImage";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    UploadImage,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/UploadImage.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <v-row no-gutters justify="center" align="center">
+      <v-col cols="8">
+        <v-file-input
+          show-size
+          label="Select Image"
+          accept="image/*"
+          @change="selectImage"
+        ></v-file-input>
+      </v-col>
+
+      <v-col cols="4" class="pl-2">
+        <v-btn color="success" dark small @click="upload">
+          Upload
+          <v-icon right dark>mdi-cloud-upload</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <div v-if="progress">
+      <div>
+        <v-progress-linear
+          v-model="progress"
+          color="light-blue"
+          height="25"
+          reactive
+        >
+          <strong>{{ progress }} %</strong>
+        </v-progress-linear>
+      </div>
+    </div>
+
+    <div v-if="previewImage">
+      <div>
+        <img class="preview my-3" :src="previewImage" alt="" />
+      </div>
+    </div>
+
+    <v-alert v-if="message" border="left" color="blue-grey" dark>
+      {{ message }}
+    </v-alert>
+
+    <v-card v-if="imageInfos.length > 0" class="mx-auto">
+      <v-list>
+        <v-subheader>List of Images</v-subheader>
+        <v-list-item-group color="primary">
+          <v-list-item v-for="(image, index) in imageInfos" :key="index">
+            <a :href="image.url">{{ image.name }}</a>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import UploadService from "../services/UploadFilesService";
+
+export default {
+  name: "upload-image",
+  data() {
+    return {
+      currentImage: undefined,
+      previewImage: undefined,
+
+      progress: 0,
+      message: "",
+
+      imageInfos: [],
+    };
+  },
+  methods: {
+    selectImage(image) {
+      this.currentImage = image;
+      this.previewImage = URL.createObjectURL(this.currentImage);
+      this.progress = 0;
+      this.message = "";
+    },
+
+    upload() {
+      if (!this.currentImage) {
+        this.message = "Please select an Image!";
+        return;
+      }
+
+      this.progress = 0;
+
+      UploadService.upload(this.currentImage, (event) => {
+        this.progress = Math.round((100 * event.loaded) / event.total);
+      })
+        .then((response) => {
+          this.message = response.data.message;
+          return UploadService.getFiles();
+        })
+        .then((images) => {
+          this.imageInfos = images.data;
+        })
+        .catch((err) => {
+          this.progress = 0;
+          this.message = "Could not upload the image! " + err;
+          this.currentImage = undefined;
+        });
+    },
+  },
+  mounted() {
+    UploadService.getFiles().then((response) => {
+      this.imageInfos = response.data;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.preview {
+  max-width: 200px;
+}
+</style>
